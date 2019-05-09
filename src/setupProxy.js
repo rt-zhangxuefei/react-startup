ed915329056a528f789ae361382ebb776bae0650
zhangxuefei <mszhangxuefei@qq.com>
2019-05-09 09:18:04 +0800

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
-  // app.use(proxy('/api', { target: 'http://localhost:3001/' }))
+  // app.use(proxy('/api', { target: 'http://localhost:3001/', changeOrigin:true, logLevel: 'debug' }))
 }
