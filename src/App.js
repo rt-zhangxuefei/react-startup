a7eb6e633ca6ec3923443042c0fffbe2b66461ae
zhangxuefei <mszhangxuefei@qq.com>
2019-05-23 18:42:27 +0800

    rename pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Redirect
 } from 'react-router-dom';
 import { CONTEXT } from './config';
-import Index from './containers/Index/Index';
+import Index from './pages/Index/Index';
 
 class App extends Component {
   render() {
