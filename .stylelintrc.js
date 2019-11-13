2a176e812aeed5390786e1a0c58a41230d143257
zhangxuefei <mszhangxuefei@qq.com>
2019-11-13 23:05:14 +0800

    update

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-rational-order',
+    'stylelint-config-prettier',
+  ].map(key => require.resolve(key)),
+  plugins: [
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties',
+  ].map(key => require.resolve(key)),
+  rules: {
+    'no-descending-specificity': null,
+    'plugin/declaration-block-no-ignored-properties': true,
+  },
+};
