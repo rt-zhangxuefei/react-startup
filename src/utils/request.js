038b37a8e2a26d8d6d020fd1c5a507f44eaf3d72
zhangxuefei <mszhangxuefei@qq.com>
2020-03-01 18:08:39 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,11 @@
+// 封装 axios 刷新 token ，代码仅供参考
+
 import axios from 'axios';
 
 const TOKEN_HEADER = 'X-TOKEN';
 
 const instance = axios.create({
-  headers: { TOKEN_HEADER: window.localStorage.getItem(TOKEN_HEADER) },
+  headers: { [TOKEN_HEADER]: window.localStorage.getItem(TOKEN_HEADER) || '' },
 });
 
 instance.setToken = token => {
@@ -14,12 +16,13 @@ instance.setToken = token => {
 let isRefreshing = false;
 // token 过期时的请求队列
 let requestQueue = [];
-
+// 重试次数
 let retryTokenTimes = 0;
 
+// 模拟异步或同步获取 getToken
 async function getToken() {
-  const response = await axios.get('token_url');
-  return response.data;
+  const response = await 'token';
+  return response;
 }
 
 instance.interceptors.response.use(
@@ -28,34 +31,27 @@ instance.interceptors.response.use(
     if (error.response.status === 401) {
       const { config } = error.response;
       // token 过期
-      if (!isRefreshing && retryTokenTimes < 4) {
+      if (!isRefreshing && retryTokenTimes < 3) {
         isRefreshing = true;
         return getToken()
           .then(res => {
-            const { data, code } = res;
-            if (code === 0) {
-              // 获取 token 成功
-              retryTokenTimes = 0;
-              instance.setToken(data);
-              config.headers[TOKEN_HEADER] = data;
-              // token 更新 执行过期请求队列
-              requestQueue.forEach(cb => cb(data));
-              requestQueue = [];
-            } else {
-              retryTokenTimes += 1;
-            }
+            isRefreshing = false;
+            // 获取 token 成功
+            retryTokenTimes = 0;
+            instance.setToken(res);
+            config.headers[TOKEN_HEADER] = res;
+            // token 更新 执行过期请求队列
+            requestQueue.forEach(cb => cb(res));
+            requestQueue = [];
             return instance(config);
           })
           .catch(err => {
+            isRefreshing = false;
             retryTokenTimes += 1;
             console.log(`getToken catch error ${err.message}`);
-          })
-          .finally(() => {
-            isRefreshing = false;
           });
       }
-
-      if (retryTokenTimes < 4) {
+      if (isRefreshing && retryTokenTimes < 3) {
         return new Promise(resolve => {
           requestQueue.push(token => {
             config.headers[TOKEN_HEADER] = token;
