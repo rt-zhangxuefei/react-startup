f211e7d6c3d1d99b6116e24ddeade6345175f260
张雪飞 <zhangxf26@lenovo.com>
2020-06-12 10:23:56 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, addWebpackPlugin } = require('customize-cra');
 
 // const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
