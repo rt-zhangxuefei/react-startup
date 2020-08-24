dbdc01227049525d984b23c23382964ae2b946eb
张雪飞 <zhangxf26@lenovo.com>
2020-08-24 14:35:17 +0800

    update

--- src/components/Authorized/AuthorizeRoute.tsx
+++ src/components/Authorized/AuthorizeRoute.tsx
@@ -1,13 +1,8 @@
 import React, { FC } from 'react';
 import { Route, RouteProps } from 'react-router-dom';
 
-const AuthorizeRoute: FC<RouteProps> = ({
-  children,
-  // noMath = <Result status="403" title="403" subTitle="抱歉，您没有权限访问当前页面"></Result>,
-  ...rest
-}) => {
+const AuthorizeRoute: FC<RouteProps> = ({ children, ...rest }) => {
   // 检查是否登录
-
   return <Route {...rest} render={() => children}></Route>;
 };
 
