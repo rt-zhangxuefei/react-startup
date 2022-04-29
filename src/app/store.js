194ab8d320928c5b589ba5b4221c336c7e639a02
张雪飞(zhangxuefei) <zhangxuefei@longfor.com>
2022-04-29 14:11:38 +0800

    调整store目录

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../pages/counter/counterSlice';
+import counterReducer from './slice/counterSlice';
 
 export default configureStore({
   reducer: {
--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,15 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root'),
+// );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
